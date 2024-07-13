d69413a552d1839c452189f05025b935aab8e2e1
Marcos Castro Abarca <mcastroabarca@gmail.com>
2024-07-13 11:00:00 -0400

    corrige error de cors

--- pos/app.js
+++ pos/app.js
@@ -1,7 +1,9 @@
-fetch('https://script.google.com/macros/s/AKfycbzXVOyaP01RJychikKUxxVbdbsSXHMDf_q3WE4jTw5obWbfyrfImYGJVzxv0iFIKwKh/exec')
-  .then(response => response.json())
-  .then(data => {
+function jsonpCallback(data) {
     const appDiv = document.getElementById('app');
     appDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
-  })
-  .catch(error => console.error('Error:', error));
+  }
+  
+  const script = document.createElement('script');
+  script.src = 'https://script.google.com/macros/s/AKfycbzXVOyaP01RJychikKUxxVbdbsSXHMDf_q3WE4jTw5obWbfyrfImYGJVzxv0iFIKwKh/exec?callback=jsonpCallback';
+  document.body.appendChild(script);
+  
