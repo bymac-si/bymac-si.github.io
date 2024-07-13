20ddfe73e418164c587782d28d38ed623bba768c
Marcos Castro Abarca <mcastroabarca@gmail.com>
2024-07-13 11:05:50 -0400

    mejorando la obtención de data

--- pos/app.js
+++ pos/app.js
@@ -3,7 +3,10 @@ function jsonpCallback(data) {
     appDiv.innerHTML = '<pre>' + JSON.stringify(data, null, 2) + '</pre>';
   }
   
-  const script = document.createElement('script');
-  script.src = 'https://script.google.com/macros/s/AKfycbzXVOyaP01RJychikKUxxVbdbsSXHMDf_q3WE4jTw5obWbfyrfImYGJVzxv0iFIKwKh/exec?callback=jsonpCallback';
-  document.body.appendChild(script);
+  function fetchData() {
+    const date = document.getElementById('dateInput').value;
+    const script = document.createElement('script');
+    script.src = `https://script.google.com/macros/s/AKfycbzXVOyaP01RJychikKUxxVbdbsSXHMDf_q3WE4jTw5obWbfyrfImYGJVzxv0iFIKwKh/exec?callback=jsonpCallback&date=${date}`;
+    document.body.appendChild(script);
+  }
   
